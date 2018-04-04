3c8712ba17ba50a36e061b5a4d0d7d605a827691
Mano_X.A <alemarquesdba@gmail.com>
2018-04-03 22:51:16 -0300

    Inclusão das dependências JPA e MySQL no projeto.

--- src/main/java/br/com/development/UdemyProjectCourseApplication.java
+++ src/main/java/br/com/development/UdemyProjectCourseApplication.java
@@ -1,12 +1,26 @@
 package br.com.development;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class UdemyProjectCourseApplication {
 
+	@Value("${paginacao.qtd_por_pagina}")
+	private int qtdPagina;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(UdemyProjectCourseApplication.class, args);
 	}
+	
+	@Bean
+	public CommandLineRunner commandLineRunner() {
+		return args -> {
+			System.out.println("### Quantidade de elementos por pagina: " + this.qtdPagina + " ###");
+		};
+	}
+	
 }
